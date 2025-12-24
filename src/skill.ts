658397e37a7eb95a25d7a9ed5483381187fc3afb
Juan Carlos <jc.campbellg@gmail.com>
2025-12-24 14:35:21 -0600

    add tv skill

--- src/skill.ts
+++ src/skill.ts
@@ -3,11 +3,13 @@ import LaunchRequestHandler from '@/handlers/LaunchRequestHandler'
 import SessionEndedRequestHandler from '@/handlers/SessionEndedRequestHandler'
 import FallbackHandler from '@/handlers/FallbackHandler'
 import MovieSearchIntentHandler from './handlers/MovieSearchIntentHandler'
+import TVShowSearchIntentHandler from './handlers/TVShowSearchIntentHandler'
 
 export const skill = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
     LaunchRequestHandler,
     MovieSearchIntentHandler,
+    TVShowSearchIntentHandler,
     FallbackHandler,
     SessionEndedRequestHandler
   )
